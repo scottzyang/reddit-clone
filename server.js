9e26ded45bfcb39549ad6e5f63c9a6d43275b383
scottzyang <scottzyang@gmail.com>
2023-04-11 16:49:57 -0500

    Ad MVC for posts

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+// require libraries
 const express = require('express');
 const { engine } = require('express-handlebars');
 
+// set up app
 const app = express();
 
+// Middleware
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// require controllers
+require('./controllers/posts')(app);
+
+// link database
+require('./data/reddit-db');
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -13,27 +24,12 @@ app.get('/', (req, res) => {
 
 // CASES RESOURCE
 
-
-// NEW
-app.get('/cases/new', (req, res) => {
-  res.render('cases-new', {});
-})
-
 // CREATE
 app.post('/cases', (req, res) => {
   console.log("We did it")
 
   res.redirect(`/cases/${caseId}`)
 });
-
-// SHOW
-
-// EDIT
-
-// UPDATE
-
-// DESTROY
-
 
 
 app.listen(3000);
