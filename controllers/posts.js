88b4b4642964113593c8aa34e2b7f13864ed6826
scottzyang <scottzyang@gmail.com>
2023-04-11 17:02:38 -0500

    Add get route to display form

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,11 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
+  // NEW
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new', {});
+  });
+
   // CREATE
   app.post('/posts/new', (req, res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
