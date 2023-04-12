57a15d90cb8bba3e7e93c980a38fdd97e16a090b
scottzyang <scottzyang@gmail.com>
2023-04-11 22:50:38 -0500

    Add index route and update deprecated mongoose syntax

--- data/reddit-db.js
+++ data/reddit-db.js
@@ -3,19 +3,25 @@ const mongoose = require('mongoose');
 assert = require('assert');
 
 const url = 'mongodb://localhost/reddit-db';
-mongoose.connect(
-  url,
-  {
-    useNewUrlParser: true
-  },
-  // function(err, db) {
+// useNewUrlParser defaults to true as of v6. https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
+mongoose.connect(url).then(() => {
+  console.log("Connected Successfully.")
+}).catch(error => console.log(error));
+
+// implemenation has changed for v7 so success handling below is no longer applicable
+  // (err) => {
   //   assert.equal(null, err);
-  //   console.log('Connected successfully to database');
+  //   console.log("Connected successfully to database");
 
   //   // db.close(); turn on for testing
   // }
-);
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+
+  mongoose.connection.on('error', err => {
+    console.error(`MongoDB connection Error: ${err}`);
+  });
+
+// connection error handling below is deprecated as of v7
+// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 mongoose.set('debug', true);
 
 module.exports = mongoose.connection;
