c6c0087fcb1acf20da910366a5b1d4d3032f8a08
scottzyang <scottzyang@gmail.com>
2023-04-19 17:35:49 -0500

    Add subreddit functionality

--- models/post.js
+++ models/post.js
@@ -4,7 +4,8 @@ const { Schema, model } = require('mongoose');
 const postSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
-  summary: { type: String, required: true }
+  summary: { type: String, required: true },
+  subreddit: { type: String, required: true }
 }, { timestamps: true });
 
 // based off the new schema, return a constructor that creates new documents.
