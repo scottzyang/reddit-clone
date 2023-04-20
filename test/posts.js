055183d619c31334f866990a1b8a75b5c4ce8a8c
scottzyang <scottzyang@gmail.com>
2023-04-20 17:56:47 -0500

    Add initial testing for authenticaiton

--- test/posts.js
+++ test/posts.js
@@ -1,7 +1,7 @@
 // test/posts.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it } = require('mocha');
+const { describe, it, after, before } = require('mocha');
 chai.use(chaiHttp);
 const app = require('../server');
 const agent = chai.request.agent(app);
@@ -9,15 +9,37 @@ const agent = chai.request.agent(app);
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
+const User = require('../models/user')
 
 const should = chai.should();
 
 describe('Posts', () => {
+  const newUser = {
+    username: "testone",
+    password: "password"
+  }
+
+  // create new user, and logs in. Created post will then be associated with this user.
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send(newUser)
+      .then((res) => {
+        done()
+      })
+      .catch((err) => {
+        done(err)
+      })
+  })
+
   const newPost = {
     title: "Post Title Test",
     url: "https://www.post-test.com",
     summary: "post summary",
+    subreddit: "testreddit",
   };
+
   it('should create a valid post and save to database at POST /posts/new', (done) => {
     Post.estimatedDocumentCount() // get number of existing docs
       .then((initialDocCount) => { // number of docs passed in to then
@@ -46,6 +68,15 @@ describe('Posts', () => {
   });
 
   after(() => {
-    Post.findOneAndDelete(newPost);
+    // Post.findOneAndDelete(newPost);
+    Post.deleteOne({  title: newPost.title })
+      .then(() => done())
+      .catch((err) => done(err));
+
+    User.deleteOne({  username: newUser.username })
+      .then(() => done())
+      .catch((err) => done(err));
+
+    agent.close();
   })
 });
