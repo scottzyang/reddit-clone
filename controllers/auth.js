37db8fbd66275bb0ff664dd2935b9e9c4e47e26d
scottzyang <scottzyang@gmail.com>
2023-04-19 23:18:49 -0500

    Add login controller and form

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,8 +23,36 @@ module.exports = (app) => {
   });
 
   // Log out
-  app.get('/logout', async (req, res) => {
+  app.get('/logout', (req, res) => {
     res.clearCookie('nToken');
     return res.redirect('/');
-  })
+  });
+
+  // Login
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  // LOGIN
+  app.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body; // grab username and password from body
+      const user = await User.findOne({ username }, 'username password') // Find a username and return the fields username and password.
+      if (!user) { // If user doesn't exist, send error status
+        return res.status(401).send({ message: "Wrong Username or Password" });
+      }
+      user.comparePassword(password, (err, isMatch) => { // Check password against user database password
+        if (!isMatch) {
+          return res.status(401).send({ message: "Wrong Username or Password" }); // password does not match
+        }
+        const token = jwt.sign({  _id: user._id, username: user.username }, process.env.SECRET, {  // Create token
+          expiresIn: '60 days',
+        });
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true }); // Set cookie in browser and redirect to root
+        return res.redirect('/');
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
 }
