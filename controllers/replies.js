9978b4a178accd9c1b7441f0a687245cd26405e1
scottzyang <scottzyang@gmail.com>
2023-04-20 21:36:47 -0500

    im tired of 2torials

--- controllers/replies.js
+++ controllers/replies.js
@@ -22,28 +22,31 @@ module.exports = (app) => {
 
   // CREATE REPLY
   app.post('/posts/:postId/comments/:commentId/replies', (req, res) => {
-    // TURN REPLY INTO A COMMENT OBJECT
-    const reply = new Comment(req.body);
-    reply.author = req.user._id;
-    // LOOKUP THE PARENT POST
-    Post.findById(req.params.postId)
-      .then((post) => {
-        // FIND THE CHILD COMMENT
-        Promise.all([
-          reply.save(),
-          Comment.findById(req.params.commentId),
-        ])
-          .then(([reply, comment]) => {
-            // ADD THE REPLY
-            comment.comments.unshift(reply._id);
-            return Promise.all([
-              comment.save(),
-            ]);
-          })
-          .then(() => res.redirect(`/posts/${req.params.postId}`))
-          .catch(console.error);
-        // SAVE THE CHANGE TO THE PARENT DOCUMENT
-        return post.save();
-      });
+    if (req.user) {
+      const reply = new Comment(req.body);
+      reply.author = req.user._id;
+      // LOOKUP THE PARENT POST
+      Post.findById(req.params.postId)
+        .then((post) => {
+          // FIND THE CHILD COMMENT
+          Promise.all([
+            reply.save(),
+            Comment.findById(req.params.commentId),
+          ])
+            .then(([reply, comment]) => {
+              // ADD THE REPLY
+              comment.comments.unshift(reply._id);
+              return Promise.all([
+                comment.save(),
+              ]);
+            })
+            .then(() => res.redirect(`/posts/${req.params.postId}`))
+            .catch(console.error);
+          // SAVE THE CHANGE TO THE PARENT DOCUMENT
+          return post.save();
+        });
+    } else {
+      res.redirect('/login')
+    }
   });
 };
